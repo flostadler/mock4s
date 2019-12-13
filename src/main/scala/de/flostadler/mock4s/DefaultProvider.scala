5a65bcbc9b0a95526b407e096a955ce56585314a
flostadler <flrn.stdlr@gmail.com>
2019-12-13 16:15:54 +0100

    Added missing AnyVals to DefaultProvider

--- src/main/scala/de/flostadler/mock4s/DefaultProvider.scala
+++ src/main/scala/de/flostadler/mock4s/DefaultProvider.scala
@@ -3,15 +3,15 @@ package de.flostadler.mock4s
 import scala.reflect.runtime.universe.{TypeTag, typeOf}
 
 object DefaultProvider {
-  def getDefaultFor[A](tag: TypeTag[A]): A = {
-    tag.tpe match {
-      case tag if tag =:= typeOf[Int] => 0.asInstanceOf[A]
-      case tag if tag =:= typeOf[Long] => 0L.asInstanceOf[A]
-      case tag if tag =:= typeOf[Short] => 0.asInstanceOf[A]
-      case tag if tag =:= typeOf[Byte] => 0.asInstanceOf[A]
-      case tag if tag =:= typeOf[Char] => '0'.asInstanceOf[A]
-      case tag if tag =:= typeOf[Boolean] => false.asInstanceOf[A]
-      case _ => null.asInstanceOf[A]
-    }
-  }
+  def getDefaultFor[A](tag: TypeTag[A]): A = (tag.tpe match {
+      case tag if tag =:= typeOf[Int] => 0
+      case tag if tag =:= typeOf[Long] => 0L
+      case tag if tag =:= typeOf[Short] => 0.toShort
+      case tag if tag =:= typeOf[Byte] => 0.toByte
+      case tag if tag =:= typeOf[Char] => '0'
+      case tag if tag =:= typeOf[Boolean] => false
+      case tag if tag =:= typeOf[Double] => .0d
+      case tag if tag =:= typeOf[Float] => .0f
+      case _ => null
+    }).asInstanceOf[A]
 }
--- src/test/scala/de/flostadler/mock4s/Foo.scala
+++ src/test/scala/de/flostadler/mock4s/Foo.scala
@@ -5,12 +5,22 @@ import scala.reflect.runtime.universe.{TypeTag, typeOf}
 
 class Foo extends AnyFlatSpec{
   "test" should "pass" in {
-    test("")
-    test(4)
-    test('a')
+    val str: String = test("")
+    val i: Int = test(4)
+    val c: Char = test('a')
+    val fl: Float = test(4.0f)
+    val d: Double = test(.0d)
+    val bool: Boolean = test(true)
+    val l: Long = test(4L)
+    val sh: Short = test(0.toShort)
+    val b: Byte = test(0.toByte)
   }
 
-  def test[B](value: B)(implicit bTag: TypeTag[B]) = {
-    println(Mock4s.defaultValue(bTag))
+//  def test[B](value: B)(implicit bTag: TypeTag[B]) = {
+//    println(DefaultProvider.getDefaultFor(bTag))
+//  }
+
+  def test[B](value: B)(implicit bTag: TypeTag[B]): B = {
+    DefaultProvider.getDefaultFor(bTag)
   }
 }

f424e90c6d4d06f7f1ae96d920b516dc2bda63de
flostadler <flrn.stdlr@gmail.com>
2019-12-16 16:09:48 +0100

    Prototyping dsl

--- src/main/scala/de/flostadler/mock4s/Mock4s.scala
+++ src/main/scala/de/flostadler/mock4s/Mock4s.scala
@@ -1,31 +1,55 @@
 package de.flostadler.mock4s
 
 
+import de.flostadler.mock4s.Mock.{mock, when}
+
 import scala.reflect.runtime.universe.{TypeTag, typeOf}
 
 object Mock4s {
-  val mockedString = Mock(classOf[String]) when { _.charAt } thenDo {
+  val mockedString = mock[String] when { _.charAt}  thenDo {
     case index if index == 7 => 'd'
     case _ => 'u'
   }
 
-  val second = Mock(classOf[String]) when { _.codePointCount } thenDo {
-    case (lower, _) if lower > 5 => 7
-    case _ => -2
-  }
+  val second = mock[String] when { _.codePointAt } thenReturn 7
 
-  val third = Mock(classOf[TestClass]) when { _.three } thenDo {
-    case (1, 2, 3) => ""
-  }
+  val third = mock[TestClass] when { _.one }
+
+  mock[TestClass] when { _.getter }
+
+  mock[TestClass] when { x => x.getter _ } thenReturn "hi"
+
+
 
 }
 
 class TestClass {
   def three(a: Int, b: Int, c: Int): String = ""
+
+  def one(a: Int): Int = 4
+
+  def getter: String = ""
 }
 
-case class Mock[A](private val clazz: Class[A]) {
-  def when[B, R](func: A => B => R)(implicit tag: TypeTag[B]): MockBuilder[A, B, R] = {
+object Mock {
+  def mock[A]: Mock[A] = new Mock[A]()
+
+  def when[A, R](a: A)(func: A => () => R): MockBuilder0[A, R] = {
+
+  }
+
+  def when[A, B, R](a: A)(func: A => B => R)(implicit tag: TypeTag[B]): MockBuilder1[A, B, R] = {
+
+  }
+}
+
+class Mock[A]() {
+
+  def when[R](func: A => () => R): MockBuilder0[A, R] = {
+
+  }
+
+  def when[B, R](func: A => B => R)(implicit tag: TypeTag[B]): MockBuilder1[A, B, R] = {
 
   }
 
@@ -38,8 +62,22 @@ case class Mock[A](private val clazz: Class[A]) {
   }
 }
 
-case class MockBuilder[A, B, R](private val clazz: Class[A], private val func: A => B => R) {
+case class MockBuilder0[A, R](private val func: A => R) {
+  def thenDo(matcher: Unit => R): A = {
+
+  }
+
+  def thenReturn(value: R): A  = {
+
+  }
+}
+
+case class MockBuilder1[A, B, R](private val clazz: Class[A], private val func: A => B => R) {
   def thenDo(matcher: PartialFunction[B, R]): A = {
+
+  }
+
+  def thenReturn(value: R): A  = {
 
   }
 }
@@ -48,10 +86,18 @@ case class MockBuilder2[A, B, C, R](private val clazz: Class[A], private val fun
   def thenDo(matcher: PartialFunction[(B, C), R]): A = {
 
   }
+
+  def thenReturn(value: R): A  = {
+
+  }
 }
 
 case class MockBuilder3[A, B, C, D, R](private val clazz: Class[A], private val func: A => (B, C, D) => R ) {
   def thenDo(matcher: PartialFunction[(B, C, D), R]): A = {
+
+  }
+
+  def thenReturn(value: R): A  = {
 
   }
 }
